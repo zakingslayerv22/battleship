bd4f088775abdc1ebbec4144ac70d80ee099f39c
Eriwe-li <11926638+zakingslayerv22@users.noreply.github.com>
2025-05-01 08:19:55 -0400

    Setup for horizontal and vertical placement of ship
    In GameBoard, add a direction
    instance property because
    the ships can be placed
    either vertically or
    horizontally.
    
    Initialize a private
    placeVertically method
    that places the ship
    vertically on the board.
    The method takes in the
    ship and its and
    its destination
    coordinates.
    It loops through the
    prospective vertical
    position that the ship
    would be placed and
    checks if there's a
    ship in any of the
    positions (considering
    the length of the
    ship).
    If no ship is in any
    of the positions, it
    loops through all those
    positions, places
    the ship
    in those positions
    according to the
    ship's length and
    returns true.
    
    Implement similar
    logic for the
    private method
    placeHorizontally
    for horizontal
    placement of ships.

--- gameboard.js
+++ gameboard.js
@@ -5,6 +5,32 @@ export class GameBoard {
     this.board = Array.from({ length: 10 }, () =>
       Array.from({ length: 10 }, () => new BoardSquare())
     );
+
+    this.direction = "vertical";
+  }
+
+  #placeVertically(shipToPlace, x, y) {
+    for (let i = x; i < x + shipToPlace.length; i++) {
+      if (this.board[i][y].ship) return false;
+    }
+
+    for (let i = x; i < x + shipToPlace.length; i++) {
+      this.board[i][y].ship = shipToPlace;
+    }
+
+    return true;
+  }
+
+  #placeHorizontally(shipToPlace, x, y) {
+    for (let i = y; i < y + shipToPlace.length; i++) {
+      if (this.board[x][i].ship) return false;
+    }
+
+    for (let i = y; i < y + shipToPlace.length; i++) {
+      this.board[x][i].ship = shipToPlace;
+    }
+
+    return true;
   }
 
   placeShip(length, x, y) {
