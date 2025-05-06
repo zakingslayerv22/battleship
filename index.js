989b0b3d2f5b29382d7456efd4af0594344439c9
Eriwe-li <11926638+zakingslayerv22@users.noreply.github.com>
2025-05-06 08:03:01 -0400

    Render boards
    In index.js, import
    the DomController and
    the
    Human and Computer
    players.
    
    Create new instances
    of three of them.
    
    Select the human and
    computer board
    containers and
    render them by calling
    the DomController's
    renderBoard with the
    right arguments.
    
    Add styles in the
    style.css so that the
    boards' squares
    are arranged in
    10x10 grids.

---
+++ index.js
@@ -0,0 +1,12 @@
+import { DomController } from "./dom-controller.js";
+import { Human, Computer } from "./player.js";
+
+const domController = new DomController();
+const humanPlayer = new Human();
+const computerPlayer = new Computer();
+
+const humanBoardContainer = document.querySelector(".human-board");
+const computerBoardContainer = document.querySelector(".computer-board");
+
+domController.renderBoard(humanPlayer, humanBoardContainer);
+domController.renderBoard(computerPlayer, computerBoardContainer);
