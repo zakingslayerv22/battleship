20e301da26f096017ed86e106aa50ccc9280b04f
Eriwe-li <11926638+zakingslayerv22@users.noreply.github.com>
2025-05-18 09:37:14 -0400

    Add styles and update display
    Add styles for the class
    that was applied to
    all cells that contains
    ships that have been hit.
    
    In the index.js
    update the display
    of the boards by
    launching human attack
    and
    rendering missed attacks
    and ships' hits
    for both players.

--- index.js
+++ index.js
@@ -17,3 +17,11 @@ computerPlayer.gameboard.populateWithDefaultShips();
 console.log(domController.renderShips("human", humanPlayer));
 
 console.log(computerPlayer.gameboard.launchComputerAttack(humanPlayer));
+console.log(humanPlayer.gameboard.receiveAttack(9, 6));
+console.log(humanPlayer.gameboard.launchHumanAttack(computerPlayer, [0, 0]));
+
+domController.renderMissedAttacks("human", humanPlayer);
+domController.renderMissedAttacks("computer", computerPlayer);
+
+domController.renderShipsHits("human", humanPlayer);
+domController.renderShipsHits("computer", computerPlayer);
