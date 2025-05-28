0fee6d29853a57c9cc70ed87a55f10e558db70b0
Eriwe-li <11926638+zakingslayerv22@users.noreply.github.com>
2025-05-28 09:49:40 -0400

    Test human and computer attacks
    Change the structure of the
    tests and test for human
    and computer attacks
    differently.

--- game.test.js
+++ game.test.js
@@ -4,67 +4,125 @@ describe("test game logic", () => {
   let newGame;
 
   const mockHumanAttack = jest.fn().mockReturnValue("Hit");
-  const mockComputerAttack = jest.fn().mockReturnValue("Hit");
+  const mockComputerAttack = jest.fn()
+    .mockReturnValueOnce("Hit")
+    .mockReturnValueOnce("Missed");
   const mockUpdateBoard = jest.fn();
+  const humanShipsSunk = jest.fn().mockReturnValue(false)
+  const computerShipsSunk = jest.fn().mockReturnValue(false)
 
   const humanPlayer = {
     gameboard: {
       launchHumanAttack: mockHumanAttack,
+      allShipsSunk : humanShipsSunk
+      
     },
   };
 
   const computerPlayer = {
     gameboard: {
       launchComputerAttack: mockComputerAttack,
+      allShipsSunk : computerShipsSunk
     },
   };
 
   beforeEach(() => {
     jest.clearAllMocks(); // reset call history before each test
-    newGame = new Game(humanPlayer, computerPlayer, mockUpdateBoard); // fresh Gamebefore every test
+    newGame = new Game(humanPlayer, computerPlayer); // fresh Gamebefore every test
+    humanShipsSunk.mockReturnValue(false);
+    computerShipsSunk.mockReturnValue(false)
   });
+  
+  describe("test human move", () => {
+    test("launches human attacks and calls the updateBoard callback correctly", () => {
+      newGame.playHumanMove([3, 5], mockUpdateBoard);
+  
+      // 1. Check human attack called with correct args
+      expect(mockHumanAttack).toHaveBeenCalledWith(computerPlayer, [3, 5]);
+  
+      // 2. Check order and args of update board
+      expect(mockUpdateBoard).toHaveBeenNthCalledWith(1, "computer");
+  
+    });
+  
+    test("returns if computer ships are sunk before move", () => {
+      computerShipsSunk.mockReturnValue(true);
+  
+      const result = newGame.playHumanMove([3, 5], mockUpdateBoard);
+  
+      expect(mockHumanAttack).not.toHaveBeenCalled()
+      expect(result).toBe("Human Wins")
+    })
+  
+    test("returns if computer ships are sunk after move", () => {
 
-  test("launches human and computer attacks and calls the updateBoard callback correctly", () => {
-    newGame.playMove([3, 5], mockUpdateBoard);
+      newGame.playHumanMove([3, 5], mockUpdateBoard);
+      computerShipsSunk.mockReturnValue(true);
+      const result = newGame.playHumanMove([3, 7], mockUpdateBoard);
+  
+  
+      expect(mockHumanAttack).toHaveBeenCalledTimes(1)
+      expect(result).toBe("Human Wins")
+    })
+  })
 
-    // 1. Check human attack called with correct args
-    expect(mockHumanAttack).toHaveBeenCalledWith(computerPlayer, [3, 5]);
+  describe("test computer moves", () => {
 
-    // 2. Check order and args of update board
-    expect(mockUpdateBoard).toHaveBeenNthCalledWith(1, "computer");
-    expect(mockUpdateBoard).toHaveBeenNthCalledWith(2, "human");
+ 
 
-    // 3. Check computer attack called
-    expect(mockComputerAttack).toHaveBeenCalledWith(humanPlayer);
+    test("launches computer attacks and calls the updateBoard callback correctly", () => {
+      jest.useFakeTimers(); //pause real timers
 
-    // 4. Check update board called twice
-    expect(mockUpdateBoard).toHaveBeenCalledTimes(2);
-  });
+      newGame.keepComputerAttacking(mockUpdateBoard);
+  
+      // 1. Check computer attack called with correct args
+      expect(mockComputerAttack).toHaveBeenCalledWith(humanPlayer);
+  
+      // 2. Check order and args of update board
+      expect(mockUpdateBoard).toHaveBeenNthCalledWith(1, "human");
+    });
+    
 
-  test("does not proceed if human move is invalid", () => {
-    mockHumanAttack.mockReturnValue(false);
+    test("returns if human ships are sunk before move", () => {
+      humanShipsSunk.mockReturnValue(true);
+  
+      const result = newGame.keepComputerAttacking(mockUpdateBoard);
+  
+      expect(mockHumanAttack).not.toHaveBeenCalled()
+      expect(result).toBe("Computer Wins")
+    });
+  
+    test("returns if computer ships are sunk after move", () => {
+      newGame.keepComputerAttacking(mockUpdateBoard);
+      humanShipsSunk.mockReturnValue(true);
+      const result = newGame.keepComputerAttacking(mockUpdateBoard);
+  
+  
+      expect(mockComputerAttack).toHaveBeenCalledTimes(1)
+      expect(result).toBe("Computer Wins")
+    })
 
-    newGame.playMove([3, 5], mockUpdateBoard);
+    test("keeps attacking if result is not missed", () => {
 
-    //Human attack was attempted
-    expect(mockHumanAttack).toHaveBeenCalled();
+      jest.useFakeTimers(); //pause real timers
 
-    // But since result was false, nothing else happens
-    expect(mockUpdateBoard).not.toHaveBeenCalled();
-    expect(mockComputerAttack).not.toHaveBeenCalled();
-  });
+      newGame.keepComputerAttacking(mockUpdateBoard);
+      
+      //initial call
+      expect(mockComputerAttack).toHaveBeenCalledTimes(1);
+      expect(mockUpdateBoard).toHaveBeenCalledTimes(1);
 
-  test("does not proceed if computer move is invalid", () => {
-    mockHumanAttack.mockReturnValue("Hit");
-    mockComputerAttack.mockReturnValue(false);
 
-    newGame.playMove([3, 5], mockUpdateBoard);
+      //after pausing real timers, 
+      //setTimeout doesn’t run now unless you advanceTimersByTime.
+      //fast-forward 2 seconds
+      jest.advanceTimersByTime(2000); 
 
-    //Computer attack was attempted
-    expect(mockComputerAttack).toHaveBeenCalled();
+      //now second attack happens
+      expect(mockComputerAttack).toHaveBeenCalledTimes(2);
+      expect(mockUpdateBoard).toHaveBeenCalledTimes(2);
 
-    // But since result was false, it does not update the human board
-    expect(mockUpdateBoard).toHaveBeenCalledTimes(1);
-    expect(mockUpdateBoard).not.toHaveBeenCalledWith("human");
-  });
+    })
+  })
+
 });
