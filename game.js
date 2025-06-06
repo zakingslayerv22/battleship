e1815ab3baa75fe7acc93b49a24511ea513a3db9
Eriwe-li <11926638+zakingslayerv22@users.noreply.github.com>
2025-06-06 12:22:28 -0400

    Remove methods
    Remove getCurrentPlayer,
    and switchPlayerTurn,
    because they are not
    needed.
    
    Remove the
    currentPlayer
    instance variable
    because it is
    also not needed.

--- game.js
+++ game.js
@@ -2,60 +2,45 @@ export class Game {
   constructor(humanPlayer, computerPlayer) {
     this.humanPlayer = humanPlayer;
     this.computerPlayer = computerPlayer;
-
-    // this.playersArray = [humanPlayer, computerPlayer];
-    this.currentPlayer = this.humanPlayer;
-  }
-
-  #getCurrentPlayer() {
-    return this.currentPlayer;
-  }
-
-  #switchPlayerTurn() {
-    this.currentPlayer =
-      this.currentPlayer === this.humanPlayer
-        ? this.computerPlayer
-        : this.humanPlayer;
   }
 
   playHumanMove([x, y], handleBoardUpdate) {
     //check before attack
-    if(this.computerPlayer.gameboard.allShipsSunk()) return "Human Wins"
+    if (this.computerPlayer.gameboard.allShipsSunk()) return "Human Wins";
 
-    const result = this.humanPlayer.gameboard.launchHumanAttack(this.computerPlayer, [x, y])
+    const result = this.humanPlayer.gameboard.launchHumanAttack(
+      this.computerPlayer,
+      [x, y]
+    );
 
     if (result) {
       console.log("Human played", result);
       handleBoardUpdate("computer");
 
       //check after attack
-      if(this.computerPlayer.gameboard.allShipsSunk()) return "Human Wins";
+      if (this.computerPlayer.gameboard.allShipsSunk()) return "Human Wins";
     }
 
-    return result
-
+    return result;
   }
-  
+
   keepComputerAttacking(handleBoardUpdate) {
     //check before attack
-    if(this.humanPlayer.gameboard.allShipsSunk()) return "Computer Wins";
-    
-    const result = this.computerPlayer.gameboard.launchComputerAttack(this.humanPlayer);
+    if (this.humanPlayer.gameboard.allShipsSunk()) return "Computer Wins";
+
+    const result = this.computerPlayer.gameboard.launchComputerAttack(
+      this.humanPlayer
+    );
     handleBoardUpdate("human");
-    
+
     //check after attack
-    
-    if(this.humanPlayer.gameboard.allShipsSunk()) return "Computer Wins";
+
+    if (this.humanPlayer.gameboard.allShipsSunk()) return "Computer Wins";
 
     if (result !== "Missed") {
       setTimeout(() => this.keepComputerAttacking(handleBoardUpdate), 2000);
     }
 
-    return result
+    return result;
   }
-
 }
-
-// const game = new Game();
-
-// console.log(game.getCurrentPlayer());
